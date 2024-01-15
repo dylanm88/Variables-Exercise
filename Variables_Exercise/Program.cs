a50e5291c1ce1cb270f83c3472f739f5c89938be
Dylan Merrill <dylanm23@msn.com>
2024-01-15 05:18:28 -0500

    Completed Variables Exercise

---
+++ Variables_Exercise/Program.cs
@@ -0,0 +1,16 @@
+﻿// Declare and initialize variables
+string name = "Noel";
+int age = 71;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 88.8;
+decimal currentPrice = 24.44m;
+
+// Writing the variables to the console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"IsOver18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
